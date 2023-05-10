7230266f4a7f732fed82c1b2eebc824269546617
Jordan Martinellli <jordan@pyramidanatomy.com>
2023-05-11 01:43:11 +0200

    New features ready

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,7 +1,6 @@
 import './Modal.css'
-import React, {useEffect, useLayoutEffect, useState} from 'react'
+import React, {CSSProperties, ReactNode, useCallback, useEffect, useLayoutEffect, useState} from 'react'
 import {createPortal} from "react-dom";
-import {ModalChildProps} from "../../index";
 
 function createWrapper(wrapperId: string) {
   const wrapperElement = document.createElement('div')
@@ -43,52 +42,104 @@ const ReactPortal = ({ children, wrapperId = 'react-portal-wrapper' }: ReactPort
 }
 
 
+type CloseHandlerPosition = {
+    top?: string
+    right?: string
+}
 
 type ModalProps = {
-  children: React.ReactNode | ((props: ModalChildProps) => React.ReactNode);
-  modalId?: string
-  trigger: React.ReactNode
-  onClose?: () => void
-  customBG?: React.CSSProperties
-  customBtn?: React.CSSProperties
+    children: ReactNode | ((props: { closeModal: () => void }) => ReactNode);
+    trigger: ReactNode
+    modalId?: string
+    backgroundColor?: string
+    onClose?: () => void
+    positionY?: 'center' | 'top' | 'bottom'
+    positionX?: 'center' | 'left' | 'right'
+    closeComponent?: ReactNode
+    closeComponentPosition?: CloseHandlerPosition
+    closeIconColor?: string
 }
 
-const Modal: React.FC<React.PropsWithChildren<ModalProps>> = ({
-  children,trigger, onClose, modalId = 'new-modal', customBG, customBtn}): React.ReactElement | null => {
-
-  const [isModalOpen, setIsModalOpen] = useState(false)
-  const handleClose = () => {
-    setIsModalOpen(false)
-    if (onClose) onClose()
-  }
-  const handleOpen = () => setIsModalOpen(true)
-
-  useEffect(() => {
-    const closeOnEscapeKey = (e: KeyboardEvent) => (e.key === 'Escape' ? handleClose() : null)
-    document.body.addEventListener('keydown', closeOnEscapeKey)
-    return () => {
-      document.body.removeEventListener('keydown', closeOnEscapeKey)
+const validateProps = (props: ModalProps) => {
+    // check if background color is valid
+    const validColor = (color: string) => {
+        const s = new Option().style;
+        s.color = color;
+        return s.color !== '';
     }
-  }, [handleClose])
-
-
-  return (
-      <>
-        <div onClick={handleOpen}>{trigger}</div>
-        {isModalOpen && (
-          <ReactPortal wrapperId={modalId}>
-            <dialog onClick={handleClose} className='bg' style={customBG}>
-              <div onClick={(e) => e.stopPropagation()} className='container'>
-                <button onClick={handleClose} className='closeBtn' style={customBtn}>
-                  ✕
-                </button>
-                {typeof children === "function" ? children({ closeModal: handleClose }) : children}
-              </div>
-            </dialog>
-          </ReactPortal>
-        )}
-      </>
-  )
+    if (!validColor(props.backgroundColor || '')) {
+        throw new Error('Invalid Modal background color')
+    }
+    // check if position is valid
+    if (!['center', 'top', 'bottom'].includes(props.positionY || '')) {
+        throw new Error('Invalid Modal positionY')
+    }
+    if (!['center', 'left', 'right'].includes(props.positionX || '')) {
+        throw new Error('Invalid Modal positionX')
+    }
 }
 
-export default Modal
+const CloseButton = ({ color = '#1F1F1F'}: { color?: string}) => (
+    <button className='defaultCloseIcon' style={{ color }}>✕</button>
+)
+
+const Modal: React.FC<ModalProps> = ({
+                                         children,
+                                         modalId = 'modal',
+                                         closeIconColor,
+                                         trigger,
+                                         closeComponent,
+                                         closeComponentPosition = { top: '24px', right: '24px' },
+                                         onClose,
+                                         backgroundColor = 'rgba(0, 0, 0, 0.42)',
+                                         positionX = 'center',
+                                         positionY = 'center'
+                                     }) => {
+
+    useEffect(() => {
+        validateProps({ children, trigger, modalId, backgroundColor, onClose, positionY, positionX, closeComponent, closeComponentPosition, closeIconColor });
+    }, [children, trigger, modalId, backgroundColor, onClose, positionY, positionX, closeComponent, closeComponentPosition, closeIconColor]);
+
+    const [isModalOpen, setIsModalOpen] = useState(false)
+    const handleClose = useCallback(() => {
+        setIsModalOpen(false)
+        if (onClose) onClose()
+    }, [onClose])
+
+    useEffect(() => {
+        const closeOnEscapeKey = (e: KeyboardEvent) => (e.key === 'Escape' ? handleClose() : null)
+        document.body.addEventListener('keydown', closeOnEscapeKey)
+        return () => {
+            document.body.removeEventListener('keydown', closeOnEscapeKey)
+        }
+    }, [handleClose])
+
+    // handle custom position by setting className
+    const modalPositionClass = `position-${positionY}-${positionX}`
+
+    const dialogStyle: CSSProperties = {
+        backgroundColor
+    }
+
+    return (
+        <>
+            <div onClick={() => setIsModalOpen(true)}>{trigger}</div>
+            {isModalOpen && (
+                <ReactPortal wrapperId={modalId}>
+                    <dialog onClick={handleClose}
+                            className={`${modalPositionClass} bg`}
+                            style={dialogStyle}>
+                        <div onClick={(e) => e.stopPropagation()} className='container'>
+                            <div onClick={handleClose} className='closeHandler' style={closeComponentPosition}>
+                                {closeComponent ? closeComponent : <CloseButton color={closeIconColor}/>}
+                            </div>
+                            {typeof children === "function" ? children({ closeModal: handleClose }): children}
+                        </div>
+                    </dialog>
+                </ReactPortal>
+            )}
+        </>
+    )
+}
+
+export default Modal;
